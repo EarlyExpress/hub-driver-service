8c5bc93838e0cfeab399538cae9278225e73ca80
jun <pickjog@gmail.com>
2025-11-25 16:19:34 +0900

    first commit

---
+++ src/main/java/com/early_express/hub_driver_service/HubDriverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.early_express.hub_driver_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubDriverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubDriverServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/early_express/hub_driver_service/HubDriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.early_express.hub_driver_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubDriverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
